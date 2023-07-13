27d9dbd634f238df10dd75041996d5e8601a69fe
Kelvin Flora <kelvin.flora@gmail.com>
2023-07-13 16:43:59 -0300

    property and event binding

--- src/app/components/thoughts/create-thoughts/create-thoughts.component.ts
+++ src/app/components/thoughts/create-thoughts/create-thoughts.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-thoughts',
   templateUrl: './create-thoughts.component.html',
-  styleUrls: ['./create-thoughts.component.css']
+  styleUrls: ['./create-thoughts.component.css'],
 })
 export class CreateThoughtsComponent {
+  thought = {
+    id: '1',
+    content: 'Aprendendo Angular',
+    author: 'Dev',
+    model: '',
+  };
 
+  constructor() {}
+
+  createThought() {
+    alert('Novo pensamento criado!');
+  }
+
+  cancel() {
+    alert('A ação foi cancelada!');
+  }
 }
